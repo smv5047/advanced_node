000acf6a70bdce66e37a5dc87a6d58106ab989c4
smv5047 <smv5047@gmail.com>
2020-07-22 00:01:48 -0400

    updated forking

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+//is file running in master mode (initial boot)
+if (cluster.isMaster) {
+  //if so, fork it
+
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  //if not execute code
+} else {
+  //im a child, im going to act like a server and do nothing else
+  const express = require("express");
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hi there");
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
+  });
+
+  app.listen(4000);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hi there");
-});
-
-app.listen(4000);
