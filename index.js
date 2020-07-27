76331df44f3194fcd997a02599c8cec329243217
smv5047 <smv5047@gmail.com>
2020-07-27 10:12:35 -0400

    cluster management updated

--- index.js
+++ index.js
@@ -3,27 +3,19 @@ const crypto = require("crypto");
 process.env.UV_THREADPOOL_SIZE = 1;
 
 //is file running in master mode (initial boot)
-if (cluster.isMaster) {
-  //if so, fork it
-  //each of the below
-  cluster.fork();
-  cluster.fork();
 
-  //if not execute code
-} else {
-  //im a child, im going to act like a server and do nothing else
-  const express = require("express");
-  const app = express();
+//im a child, im going to act like a server and do nothing else
+const express = require("express");
+const app = express();
 
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("Hi there");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast!");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast!");
+});
 
-  app.listen(4000);
-}
+app.listen(4000);
---
+++ manual-clustering.js
@@ -0,0 +1,31 @@
+//Old file when performing manual clusterin
+
+const cluster = require("cluster");
+const crypto = require("crypto");
+process.env.UV_THREADPOOL_SIZE = 1;
+
+//is file running in master mode (initial boot)
+if (cluster.isMaster) {
+  //if so, fork it
+  //each of the below
+  cluster.fork();
+  cluster.fork();
+
+  //if not execute code
+} else {
+  //im a child, im going to act like a server and do nothing else
+  const express = require("express");
+  const app = express();
+
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
+  });
+
+  app.listen(4000);
+}
